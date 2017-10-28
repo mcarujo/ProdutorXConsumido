c301ddf984ae7fd6e2610e76c0f6cad12e43bc07
mcarujo <mamcarujo@gmail.com>
2017-10-28 19:08:41 -0300

    to perfection

--- main.cpp
+++ main.cpp
@@ -2,78 +2,106 @@
 #include <pthread.h>
 #include <math.h>
 #include <unistd.h>
-#include "BlackTime/BlackTime.h" 
+#include "BlackTime/BlackTime.h"
 #include "BlackGPIO/BlackGPIO.h"
 #include "ADC/Adc.h"
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
 #include "display.h"
-
- // Inclua as classes que achar necessario
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h> // biblioteca pthread
 
 #define UNIT_MS 1000
 #define UNIT_SEC 1000000
 
 using namespace std;
 
-int main(int argc, char * argv[])
-{
-	/****************** Instanciação de Variaveis ***********************/ 
+/****************** Variaveis Globais ***********************/
 	// GPIO
-    BlackLib::BlackGPIO saida(BlackLib::GPIO_67, BlackLib::output, BlackLib::SecureMode);
-    BlackLib::BlackGPIO entrada(BlackLib::GPIO_68, BlackLib::input, BlackLib::SecureMode);
+	BlackLib::BlackGPIO saida(BlackLib::GPIO_67, BlackLib::output, BlackLib::SecureMode);
+	BlackLib::BlackGPIO entrada(BlackLib::GPIO_68, BlackLib::input, BlackLib::SecureMode);
 
 	// Variáveis relacioandas ao tempo
-	uint32_t tempoRandomico, milissegundos;
-	BlackLib::BlackTime medidor;
-	BlackLib::BlackTimeElapsed tempoDecorrido;
-	
+	int numero;
+	float velocidade1,velocidade2;
 	// Display
-	Display display(BlackLib::GPIO_65, BlackLib::GPIO_45, 
-		BlackLib::GPIO_69, BlackLib::GPIO_60, BlackLib::GPIO_27, BlackLib::GPIO_66, 
-		BlackLib::GPIO_49, BlackLib::GPIO_115, 
-		BlackLib::GPIO_20, BlackLib::GPIO_47, 
-		BlackLib::GPIO_48, BlackLib::GPIO_46);
+	Display display(BlackLib::GPIO_65, BlackLib::GPIO_45,
+					BlackLib::GPIO_69, BlackLib::GPIO_60, BlackLib::GPIO_27, BlackLib::GPIO_66,
+					BlackLib::GPIO_49, BlackLib::GPIO_115,
+					BlackLib::GPIO_20, BlackLib::GPIO_47,
+					BlackLib::GPIO_48, BlackLib::GPIO_46);
 
-	/****************** Fim da Instanciação de Variaveis ***********************/    
+	void *thread_function(void *arg);
+	int controle(int numero);
 
-	while(1){
-			
-		// Fornecendo o seed
-		srand (time(NULL));
-		
-		// Configuração inicial
-		saida.setValue(BlackLib::high);
-		display.showNumber(0000);
-		
-		while(entrada.getValue() == "1");
-		
-		// Desligando a Luz para o inicio do Jogo
-		saida.setValue(BlackLib::low);
-		
-		// Gera um número aleatório entre 1 e 3 segundos e espera este tempo.
-		tempoRandomico = rand() % 3000 + 1000;
-		usleep(tempoRandomico * UNIT_MS);
+/****************** FIM de Globais ***********************/
 
-		// Pega o tempo inicial e acende a luz
-		medidor.start(); 
-		saida.setValue(BlackLib::high);
+int main()
+{
+	int res;
+	pthread_t consumdior, produtor;
+	void *thread_result;
+	string valor;
 
-		// Espera o aperto do Botão
-		while(entrada.getValue() == "1");
-
-		// Pega o tempo final
-		tempoDecorrido = medidor.elapsed();
-		milissegundos = tempoDecorrido.second*1000 + tempoDecorrido.miliSecond;
-		
-		
-		display.showNumber(milissegundos);
-		//cout << "Tempo de reação: " << milissegundos << endl;
-		//cout << "Reiniciando a contagem em 5 segundos..." << endl;
-		usleep(5 * UNIT_SEC);
+	printf("MAIN()--> Criar thread...\n");
+	valor = "consumidor";
+	res = pthread_create(&thread1, NULL, thread_function, (void *)&valor);
+	if (res != 0)
+	{
+		perror("A Craição da Thread falhou");
+		exit(EXIT_FAILURE);
 	}
-
+	valor = "produtor";
+	res = pthread_create(&thread1, NULL, thread_function, (void *)&valor);
+	if (res != 0)
+	{
+		perror("A Craição da Thread falhou");
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
 
+void *thread_function(void *valor)
+{
+	if (valor == "consumidor")
+	{
+		while (1)
+		{
+		
+		}
+	}
+	else if (valor == "produtor")
+	{
+		while (1)
+		{
+		}
+	}
+	else
+	{
+		cout << "\nDeu erro amigão!\n";
+	}
+}
+
+int controle(int numero, bool action)
+{
+	if (action)
+	{
+		numero+= 1;
+	}
+	else
+	{
+		numero-= 1;
+	}
+	if (numero > 7)
+	{
+		numero = 7;
+	}
+	if (numero < 0)
+	{
+		numero = 0;
+	}
+
+	return numero;
+}
